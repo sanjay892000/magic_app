b6a16e38d0e7f4dde3d6187eb9c8ff3fd4c8c5b1
sanjay892000 <sanjay892000@gmail.com>
2023-10-28 22:58:16 +0530

    commit

---
+++ script.js
@@ -0,0 +1,23 @@
+let myName = document.getElementById('myName');
+let myButton = document.getElementById('myButton');
+let finalResult= document.getElementById('finalResult');
+
+let array=['Pagal hai','Chamar hai','Gadha hai','kutta hai', 'Bhukkhad hai','Kamchor hai','Aalasi hai','Shwarthi hai','Matalbi hai','Darpok hai']
+myButton.addEventListener('click', ()=>{
+
+    if(myName.value!=""){
+     const res= array[Math.floor(Math.random()*array.length)];
+     finalResult.innerText=myName.value+" "+res;
+     myName.value="";
+
+ }
+ else{
+    finalResult.innerText="Please enter your name"
+ }
+
+})
+
+/* const text="";
+    text=myName.innerText;
+    console.log(text)
+    finalResult.innerHTML=text; */
